bad9bfe5a81a20a0b0abedfaed6bd042890540c5
曾超 <475383200@qq.com>
2021-03-06 21:00:18 +0800

    feat: 完成工作流封装

---
+++ bin/zc-pages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+process.argv.push('--cwd')
+process.argv.push(process.cwd())
+process.argv.push('--gulpfile')
+process.argv.push(require.resolve('..'))
+
+require('gulp/bin/gulp')
